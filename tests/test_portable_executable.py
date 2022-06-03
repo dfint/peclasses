591c3546f2e3e7ea954756988fbb94259e539a95
insolor <insolor@gmail.com>
2022-06-03 16:17:31 +0300

    Add a failing test for x64 executable file

--- tests/test_portable_executable.py
+++ tests/test_portable_executable.py
@@ -13,6 +13,23 @@ from peclasses.utilities import align
 chars = ImageSectionHeader.Characteristics
 
 
+def compile_asm(asm_code: str):
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        tmpdir = Path(tmpdirname)
+
+        asm_file_path = tmpdir / "file.asm"
+        exe_file_path = tmpdir / "file.exe"
+
+        with open(asm_file_path, "wt") as asm_file:
+            asm_file.write(asm_code)
+
+        os.system(f"fasm {asm_file_path} {exe_file_path}")
+        with open(exe_file_path, "rb") as exe_file:
+            exe_file_data = exe_file.read()
+
+        return exe_file_data
+
+
 @pytest.fixture
 def exe_file():
     asm_code = """
@@ -28,24 +45,47 @@ def exe_file():
         section '.reloc' data readable discardable fixups
         """
 
-    with tempfile.TemporaryDirectory() as tmpdirname:
-        tmpdir = Path(tmpdirname)
-        
-        asm_file_path = tmpdir / "file.asm"
-        exe_file_path = tmpdir / "file.exe"
-        
-        with open(asm_file_path, "wt") as asm_file:
-            asm_file.write(asm_code)
-        
-        os.system(f"fasm {asm_file_path} {exe_file_path}")
-        with open(exe_file_path, "rb") as exe_file:
-            exe_file_data = exe_file.read()
+    return BytesIO(compile_asm(asm_code))
 
-        yield BytesIO(exe_file_data)
+
+@pytest.fixture
+def exe64_file():
+    asm_code = """
+        format PE64 GUI
+
+        section '.text' code readable executable
+            mov rax, some_string
+            ret
+
+        section '.data' data readable writable
+            some_string db 'some string', 0
+
+        section '.reloc' data readable discardable fixups
+        """
+
+    return BytesIO(compile_asm(asm_code))
 
 
 def test_portable_executable_section_table(exe_file):
     pe = PortableExecutable(exe_file)
+    assert [(section.name, section.characteristics) for section in pe.section_table] == [
+        (
+            b'.text',
+            chars.IMAGE_SCN_CNT_CODE | chars.IMAGE_SCN_MEM_READ | chars.IMAGE_SCN_MEM_EXECUTE
+        ),
+        (
+            b'.data',
+            chars.IMAGE_SCN_CNT_INITIALIZED_DATA | chars.IMAGE_SCN_MEM_READ | chars.IMAGE_SCN_MEM_WRITE
+        ),
+        (
+            b'.reloc',
+            chars.IMAGE_SCN_CNT_INITIALIZED_DATA | chars.IMAGE_SCN_MEM_READ | chars.IMAGE_SCN_MEM_DISCARDABLE
+        ),
+    ]
+
+
+def test_portable_executable_x64_section_table(exe64_file):
+    pe = PortableExecutable(exe64_file)
     assert [(section.name, section.characteristics) for section in pe.section_table] == [
         (
             b'.text',
