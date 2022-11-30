ad46a7db981dfa329fe24433cd6340defc6abf78
insolor <insolor@gmail.com>
2022-11-30 21:56:54 +0300

    Add implementation of the Bisector class for Python 3.10

--- peclasses/bisect_helper.py
+++ peclasses/bisect_helper.py
@@ -1,34 +1,52 @@
 import bisect
+import sys
 from typing import Sequence, Callable, TypeVar, Generic
 
 TValue = TypeVar("TValue")
 
 
-class KeySequenceWrapper(Sequence[TValue]):
-    sequence: Sequence[TValue]
-    key: Callable[[TValue], int]
+if sys.version_info[0:2] >= (3, 10):
+    class Bisector(Generic[TValue]):
+        _sequence: Sequence[TValue]
+        _key: Callable[[TValue], int]
 
-    def __init__(self, sequence: Sequence[TValue], key: Callable[[TValue], int]):
-        self.sequence = sequence
-        self.key = key
+        def __init__(self, sequence: Sequence[TValue], key: Callable[[TValue], int]):
+            self._sequence = sequence
+            self._key = key
 
-    def __len__(self) -> int:
-        return len(self.sequence)
+        def bisect_right(self, x: int, *args, **kwargs) -> int:
+            return bisect.bisect_right(self._sequence, x, *args, **kwargs, key=self._key)
 
-    def __getitem__(self, i: int) -> int:
-        return self.key(self.sequence[i])
+        bisect = bisect_right
+
+        def bisect_left(self, x: int, *args, **kwargs) -> int:
+            return bisect.bisect_left(self._sequence, x, *args, **kwargs, key=self._key)
+else:
+    class _KeySequenceWrapper(Sequence[TValue]):
+        sequence: Sequence[TValue]
+        key: Callable[[TValue], int]
+
+        def __init__(self, sequence: Sequence[TValue], key: Callable[[TValue], int]):
+            self.sequence = sequence
+            self.key = key
+
+        def __len__(self) -> int:
+            return len(self.sequence)
+
+        def __getitem__(self, i: int) -> int:
+            return self.key(self.sequence[i])
 
 
-class Bisector(Generic[TValue]):
-    _wrapper: KeySequenceWrapper[TValue]
+    class Bisector(Generic[TValue]):
+        _wrapper: _KeySequenceWrapper[TValue]
 
-    def __init__(self, sequence: Sequence[TValue], key: Callable[[TValue], int]):
-        self._wrapper = KeySequenceWrapper(sequence, key)
+        def __init__(self, sequence: Sequence[TValue], key: Callable[[TValue], int]):
+            self._wrapper = _KeySequenceWrapper(sequence, key)
 
-    def bisect_right(self, x: int, *args, **kwargs) -> int:
-        return bisect.bisect_right(self._wrapper, x, *args, **kwargs)
+        def bisect_right(self, x: int, *args, **kwargs) -> int:
+            return bisect.bisect_right(self._wrapper, x, *args, **kwargs)
 
-    bisect = bisect_right
+        bisect = bisect_right
 
-    def bisect_left(self, x: int, *args, **kwargs) -> int:
-        return bisect.bisect_left(self._wrapper, x, *args, **kwargs)
+        def bisect_left(self, x: int, *args, **kwargs) -> int:
+            return bisect.bisect_left(self._wrapper, x, *args, **kwargs)
